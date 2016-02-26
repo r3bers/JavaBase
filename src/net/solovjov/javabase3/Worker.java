e7295dfe76f07c0eba83b8aac8a56209c291a957
r3bers <mike@solovjov.net>
2016-02-26 15:23:46 +0300

    Добавил Воркера, что бы диаграммы позырить.

---
+++ src/net/solovjov/javabase3/Worker.java
@@ -0,0 +1,15 @@
+package net.solovjov.javabase3;
+
+/**
+ * Created by r3bers on 26.02.16.
+ * Рабочий
+ */
+public class Worker extends Person{
+    public Worker(String name, int age, int height) {
+        super(name, age, height);
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+}
